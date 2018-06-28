4dcfd7ee346a2ec2ee1d4e4acd785b082280f8cf
binque <binque@ya.ru>
2018-06-28 21:55:44 +0300

    password

--- web/assets/password.js
+++ web/assets/password.js
@@ -1,25 +1,3 @@
-$("#old-password, .new-password").keyup(function() {
-    var match = newPasswordsMatch();
-    var filled = formFilled();
-
-    $("#password-status").text(match ? "" : "Пароли не совпадают.")
-    $("#save-password").toggleClass("disabled", !match || !filled)
-})
-
-function newPasswordsMatch() {
-    var inputs = $(".new-password")
-    return inputs[0].attr("value") == inputs[1].attr("value")    
-}
-
-function formFilled() {
-    var inputs = $("#old-password, .new-password")
-    for(var i = 0; i < inputs.length; i++)
-        if(inputs[i].attr("value").length == 0)
-            return false;
-
-    return true;
-}
-
 $("#save-password").click(function() { 
     var status = $("#password-status")
     
