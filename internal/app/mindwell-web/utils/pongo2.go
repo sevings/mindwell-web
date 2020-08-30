7411930bb44374cd8036cbb87c4925c5ffd8f02b
binque <binque@ya.ru>
2020-08-30 19:29:01 +0300

    cut messages, notifications

--- internal/app/mindwell-web/utils/pongo2.go
+++ internal/app/mindwell-web/utils/pongo2.go
@@ -5,19 +5,23 @@ import (
 	"fmt"
 	"log"
 	"regexp"
+	"strconv"
 	"strings"
 
 	"github.com/flosch/pongo2"
+	"github.com/sevings/mindwell-server/utils"
 )
 
 func init() {
 	registerFilter("quantity", quantity)
 	registerFilter("gender", gender)
 	registerFilter("media", media)
+	registerFilter("cut_html", cutHtml)
+	registerFilter("cut_text", cutText)
 }
 
-func registerFilter(name string, filter func(*pongo2.Value, *pongo2.Value) (*pongo2.Value, *pongo2.Error)) {
-	err := pongo2.RegisterFilter(name, pongo2.FilterFunction(filter))
+func registerFilter(name string, filter pongo2.FilterFunction) {
+	err := pongo2.RegisterFilter(name, filter)
 	if err != nil {
 		log.Println(err)
 	}
@@ -147,4 +151,85 @@ func media(content *pongo2.Value, param *pongo2.Value) (*pongo2.Value, *pongo2.E
 	})
 
 	return pongo2.AsSafeValue(html), nil
+}
+
+var tagRe = regexp.MustCompile(`<[^>]+>`)
+var imgRe = regexp.MustCompile(`(?i)<img[^>]+>`)
+
+func cutHtml(content *pongo2.Value, param *pongo2.Value) (*pongo2.Value, *pongo2.Error) {
+	if content.IsNil() {
+		return content, nil
+	}
+
+	if !content.IsString() {
+		return nil, &pongo2.Error{
+			Sender:    "filter:cut_html",
+			OrigError: errors.New("input value is not a string"),
+		}
+	}
+
+	if !param.IsString() {
+		return nil, &pongo2.Error{
+			Sender:    "filter:cut_html",
+			OrigError: errors.New("parameter is not an string"),
+		}
+	}
+
+	lenLines := strings.Split(param.String(), "x")
+	if len(lenLines) < 2 {
+		return nil, &pongo2.Error{
+			Sender:    "filter:cut_html",
+			OrigError: errors.New("expected two numbers"),
+		}
+	}
+
+	maxLineLen, err := strconv.Atoi(lenLines[0])
+	if err != nil {
+		return nil, &pongo2.Error{
+			Sender:    "filter:cut_html",
+			OrigError: err,
+		}
+	}
+
+	maxLineCount, err := strconv.Atoi(lenLines[1])
+	if err != nil {
+		return nil, &pongo2.Error{
+			Sender:    "filter:cut_html",
+			OrigError: err,
+		}
+	}
+
+	html := content.String()
+	html, _ = utils.CutHtml(html, maxLineCount, maxLineLen)
+
+	return pongo2.AsSafeValue(html), nil
+}
+
+func cutText(content *pongo2.Value, param *pongo2.Value) (*pongo2.Value, *pongo2.Error) {
+	if content.IsNil() {
+		return content, nil
+	}
+
+	if !content.IsString() {
+		return nil, &pongo2.Error{
+			Sender:    "filter:cut_text",
+			OrigError: errors.New("input value is not a string"),
+		}
+	}
+
+	if !param.IsInteger() {
+		return nil, &pongo2.Error{
+			Sender:    "filter:cut_text",
+			OrigError: errors.New("parameter is not an integer"),
+		}
+	}
+
+	text := content.String()
+	maxLength := param.Integer()
+
+	text = imgRe.ReplaceAllString(text, "[изображение]")
+	text = tagRe.ReplaceAllString(text, "")
+	text, _ = utils.CutText(text, maxLength)
+
+	return pongo2.AsSafeValue(text), nil
 }
